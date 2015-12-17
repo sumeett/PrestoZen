318a30e1af6c7a66164532665643571228c8b01b
Sumeet <sthadani@ysamsungaccelerator.com>
2015-12-17 14:39:37 -0500

    1. fix bugs, compile errors 2. Get current location using GPS, and don't enable pelias search widget until we have location

--- mobile/src/main/java/prestozen/presto/watch/prestozen/MainPhoneActivity.java
+++ mobile/src/main/java/prestozen/presto/watch/prestozen/MainPhoneActivity.java
@@ -3,8 +3,10 @@ package prestozen.presto.watch.prestozen;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +16,9 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.mapzen.pelias.gson.Feature;
 import com.mapzen.pelias.gson.Result;
 import com.mapzen.pelias.widget.PeliasSearchView;
@@ -24,9 +29,13 @@ import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class MainPhoneActivity extends AppCompatActivity {
+public class MainPhoneActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final String TAG = "MainPhoneActivity";
     private PeliasSearchView searchView;
+    private GoogleApiClient googleApiClient;
+
+    private Location myLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +52,7 @@ public class MainPhoneActivity extends AppCompatActivity {
             @Override
             public void success(Result result, Response response) {
                 List<Feature> features = result.getFeatures();
-                result.getType()
-                if(features == null && features.size() < 1) {
+                if (features == null || features.size() < 1) {
                     Toast.makeText(MainPhoneActivity.this,
                             "No lat, lng found for address", Toast.LENGTH_LONG).show();
                     //TODO: reset searchView
@@ -52,6 +60,7 @@ public class MainPhoneActivity extends AppCompatActivity {
                     Feature f = features.get(0);
                     double lat = f.geometry.coordinates.get(0);
                     double lng = f.geometry.coordinates.get(1);
+
 
                 }
             }
@@ -63,5 +72,45 @@ public class MainPhoneActivity extends AppCompatActivity {
             }
         });
         searchView.setEnabled(false);//Don't enable search view util we have location
+        //get the current location
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        myLoc = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if(myLoc != null)
+            searchView.setEnabled(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "Connection suspended GPS:LocationServices");
+        Toast.makeText(MainPhoneActivity.this,
+                "Unable to get current location, connection suspended", Toast.LENGTH_LONG).show();
+        searchView.setEnabled(false);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.w(TAG, "Failed to connect with GPS:LocationServices");
+        Toast.makeText(MainPhoneActivity.this,
+                "Unable to get current location, connection failed", Toast.LENGTH_LONG).show();
+        searchView.setEnabled(false);
     }
 }
